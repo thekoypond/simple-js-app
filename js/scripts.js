56fd5dcd8e69ae56d987fe2b5329520aa0bec2b4
thekoypond <koy2300@gmail.com>
2023-02-09 14:50:05 -0500

    Added pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello world');
-let favoriteFood = 'pizza'
-document.write(favoriteFood)
+let pokemonList = [
+    { name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
+    { name: 'Charmander', height: 6, types: 'fire'},
+    { name: 'Squirtle', height: 5, types: 'water'}
+];
+
