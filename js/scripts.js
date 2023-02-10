9636579e3119e14202ae14ef7d20db8ef26ad2ca
thekoypond <koy2300@gmail.com>
2023-02-10 17:01:42 -0500

    Added loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 let pokemonList = [
-    { name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
-    { name: 'Charmander', height: 6, types: 'fire'},
-    { name: 'Squirtle', height: 5, types: 'water'}
-];
-
+    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    { name: 'Charmander', height: 0.6, types: 'fire'},
+    { name: 'Squirtle', height: 0.5, types: 'water'}
+]
+for (let i = 0; i < pokemonList.length; i++){
+        if (pokemonList[i].height > 0.6){
+            document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' +  " - Wow, that's big!"  + '<br>')
+        }
+        else {
+            document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '<br>') 
+        }
+}
