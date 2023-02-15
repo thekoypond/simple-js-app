2b2f9d371a1a7b2b22838fc4013fe81732a2e6af
thekoypond <koy2300@gmail.com>
2023-02-15 14:22:23 -0500

    Added IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
-let repository = [
-    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-    { name: 'Charmander', height: 0.6, types: 'fire'},
-    { name: 'Squirtle', height: 0.5, types: 'water'}
-]
-function printArrayDetails(pokemon){
-        if (pokemon.height > 0.6){
-            document.write(pokemon.name + ' (height: ' + pokemon.height + ')' +  " - Wow, that's big!"  + '<br>')
+let pokemonRepository = (function () {
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+        { name: 'Charmander', height: 0.6, types: 'fire'},
+        { name: 'Squirtle', height: 0.5, types: 'water'}
+    ];
+  
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+  
+    function getAll() {
+      return pokemonList;
+    }
+  
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
+
+function printArrayDetails(i){
+        if (i.height > 0.6){
+            document.write(i.name + ' (height: ' + i.height + ')' +  " - Wow, that's big!"  + '<br>')
         }
         else {
-            document.write(pokemon.name + ' (height: ' + pokemon.height + ')' + '<br>') 
+            document.write(i.name + ' (height: ' + i.height + ')' + '<br>') 
         }
 };
-repository.forEach(printArrayDetails);
+pokemonRepository.getAll().forEach(printArrayDetails);
