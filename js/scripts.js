7d2225f94129575cb128035d26e89b26b9e1cbee
thekoypond <koy2300@gmail.com>
2023-02-15 13:47:04 -0500

    Changed for loop to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
-let pokemonList = [
+let repository = [
     { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
     { name: 'Charmander', height: 0.6, types: 'fire'},
     { name: 'Squirtle', height: 0.5, types: 'water'}
 ]
-for (let i = 0; i < pokemonList.length; i++){
-        if (pokemonList[i].height > 0.6){
-            document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' +  " - Wow, that's big!"  + '<br>')
+function printArrayDetails(pokemon){
+        if (pokemon.height > 0.6){
+            document.write(pokemon.name + ' (height: ' + pokemon.height + ')' +  " - Wow, that's big!"  + '<br>')
         }
         else {
-            document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '<br>') 
+            document.write(pokemon.name + ' (height: ' + pokemon.height + ')' + '<br>') 
         }
-}
+};
+repository.forEach(printArrayDetails);
